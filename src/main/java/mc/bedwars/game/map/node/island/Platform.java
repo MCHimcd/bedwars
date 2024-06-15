0f71d5c72e0a1fdf1a15bf9690ff2830d7c3ac00
xOyMove <1178611788@qq.com>
2024-06-15 15:49:27 +0800

    6.15

---
+++ src/main/java/mc/bedwars/game/map/node/island/Platform.java
@@ -0,0 +1,13 @@
+package mc.bedwars.game.map.node.island;
+
+public class Platform extends Island{
+    public Platform(int x, int y) {
+        super(x, y);
+    }
+
+    @Override
+    public String getType() {
+        return "平台";
+    }
+
+}
